84d362985cf2d915079b291811a5d2b72df56a2c
Yitzhak Peleg <yitzhak.peleg@gmail.com>
2026-04-16 13:32:45 +0300

    Refactor connection logic in check.py to improve clarity and update host parameters

--- src/check.py
+++ src/check.py
@@ -1,7 +1,9 @@
 # %%
 import time
+
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from rich import print
 
 
 # %%
@@ -12,13 +14,17 @@ class IBapi(EWrapper, EClient):
 
 # %%
 app = IBapi()
-app.connect("host.docker.internal", 7497, 123)
 # %%
-app.run()
+app.connect(host="127.0.0.1", port=4002, clientId=123)
+print(app.isConnected())
+# %%
+# app.run()
 # %%
 # Uncomment this section if unable to connect
 # and to prevent errors on a reconnect
+for k in range(4):
+    print(f"Waiting for connection... {k + 1}/10")
+    time.sleep(1)
 
-
-time.sleep(5)
 app.disconnect()
+print(app.isConnected())
